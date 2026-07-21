ec1a7268feafd144a35cba4af00258e33237af22
claus-code-dev <claudemir.sa@hotmail.com>
2026-07-21 11:52:22 -0300

    Update api.js
    segunda atualização

--- js/api.js
+++ js/api.js
@@ -3,12 +3,12 @@ const BASE_URL = "https://claus-code-dev.github.io/salmos";
 export async function buscarSalmos() {
   try {
     const resposta = await fetch(`${BASE_URL}/salmos.json`);
-    if (!resposta.ok) throw new Error(`Erro: ${resposta.status}`);
-    
-    const dados = await resposta.json();
-    return dados;
+    if (!resposta.ok) {
+      throw new Error(`Erro na rede: status ${resposta.status}`);
+    }
+    return await resposta.json();
   } catch (erro) {
-    console.error("Erro ao carregar o arquivo de salmos:", erro);
+    console.error("Falha ao buscar o arquivo JSON:", erro);
     return null;
   }
 }
